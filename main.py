daf99c86e991bf4cc3ddd3f2adb350a1b3f89c39
KebbaPhilip <kebbaphilip@gmail.com>
2026-08-18 12:14:58 +0000

    screen setup

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from turtle import Screen, Turtle, screensize
+
+
 def main():
-    print("Hello from snake-game!")
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+
+    screen.mainloop()
 
 
 if __name__ == "__main__":
