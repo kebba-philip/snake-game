4581215a58b3c68bb9f802101e3bd122437440a0
KebbaPhilip <kebbaphilip@gmail.com>
2026-08-18 13:59:15 +0000

    import snake to main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from turtle import Screen, Turtle, screensize
+from turtle import Screen
+from snake import Snake
+import time
 
 
 def main():
@@ -6,9 +8,25 @@ def main():
     screen.setup(width=600, height=600)
     screen.bgcolor("black")
     screen.title("Snake Game")
+    screen.tracer(0)
+
+    snake = Snake()
+
+
+
+    game_is_on = True
+
+
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+
+        snake.move()
+
+
+
 
     screen.mainloop()
-
 
 if __name__ == "__main__":
     main()
