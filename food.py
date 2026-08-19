8a950550ead4c1970974f5ff064882aa966e550b
KebbaPhilip <kebbaphilip@gmail.com>
2026-08-19 11:42:14 +0000

    refresh food after collision

--- food.py
+++ food.py
@@ -10,6 +10,10 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("yellow")
         self.speed("fastest")
+
+
+
+    def refresh(self):
         x_pos = random.randint(-280, 280)
         y_pos = random.randint(-280, 280)
         self.goto(x_pos, y_pos)
--- main.py
+++ main.py
@@ -28,8 +28,10 @@ def main():
     while game_is_on:
         screen.update()
         time.sleep(0.1)
-
         snake.move()
+
+        if snake.head.distance(food) < 15:
+            food.refresh()
 
     screen.mainloop()
 
