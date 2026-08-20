3652fddffce911611f939af2e278aa514e9c4225
KebbaPhilip <kebbaphilip@gmail.com>
2026-08-20 13:05:01 +0000

    detect collision with wall

--- main.py
+++ main.py
@@ -32,9 +32,17 @@ def main():
         time.sleep(0.1)
         snake.move()
 
+        # collision with food
         if snake.head.distance(food) < 15:
             food.refresh()
             scoreboard.increase_score()
+
+        # collision with wall
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+            game_is_on = False
+            scoreboard.game_over()
+
+
 
     screen.mainloop()
 
