25be0ff90714e83206ed9a40ab4b9b2de4c38ef9
KebbaPhilip <kebbaphilip@gmail.com>
2026-08-20 13:31:52 +0000

    detect collision with snake tail or body (slicing)

--- main.py
+++ main.py
@@ -40,10 +40,8 @@ def main():
             scoreboard.game_over()
 
         # collison with snake tail or body
-        for segment in snake.segments:
-            if segment == snake.head:
-                pass
-            elif snake.head.distance(segment) < 10:
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
                 game_is_on = False
                 scoreboard.game_over()
 
